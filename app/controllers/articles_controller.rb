35e60f52ddbf76fb3fdd88ae8106b6e811590706
Sergiy Dmytrenko <dmytrenko.serhii@gmail.com>
2016-12-06 14:33:42 +0200

    Add simply authentication

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 
+  http_basic_authenticate_with name: "user", password: "123456", except: [:index, :show]
+
   def index
     @articles = Article.all
   end
